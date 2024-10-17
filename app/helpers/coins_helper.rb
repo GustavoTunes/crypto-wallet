a1f0b10f218e05bc9324e23888491c01ccce2dcd
GustavoTunes <gustavosouzatunes@gmail.com>
2024-10-17 17:12:07 -0300

    Add submit button helper

--- app/helpers/coins_helper.rb
+++ app/helpers/coins_helper.rb
@@ -1,2 +1,9 @@
 module CoinsHelper
+    def submit_button(form)
+        if form.object.persisted?
+          "Update currency "
+        else
+          "Create currency"
+        end
+    end
 end
