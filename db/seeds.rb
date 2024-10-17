78fdee872becc0a91644655accf4b9b3ac185e51
GustavoTunes <gustavosouzatunes@gmail.com>
2024-10-17 18:47:28 -0300

    Start seed file setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Registering Currencies..."
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png"
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://upload.wikimedia.org/wikipedia/commons/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png"
+)
+
+Coin.create!(
+    description: "Dogecoin",
+    acronym: "DOGE",
+    url_image: "https://cryptologos.cc/logos/dogecoin-doge-logo.png"
+)
+
+puts "Currencies registered successfully!"
