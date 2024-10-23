7b34c1d2eba764d426d5f411cf1400c9713d4f3a
GustavoTunes <gustavosouzatunes@gmail.com>
2024-10-23 17:47:56 -0300

    Update views with bootstrap

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,3 +1,8 @@
 class Coin < ApplicationRecord
     belongs_to :mining_type 
+
+    validates :url_image, format: { 
+        with: /\A(?:https?:\/\/)?(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:\/[^\s]*)?\.(?:png|jpe?g)\z/i, 
+        message: :url_image
+    }, allow_blank: true
 end
